4be6ebe79bf345bce496646c97193a9f8f02abe8
Roberto Júnior <robertojuniorn@gmail.com>
2021-02-25 21:28:02 +0200

    new changes js

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -2,12 +2,14 @@ let inputText = document.getElementById('inputText')
 let addbutton = document.getElementById('addbutton');
 let res = document.getElementById('res');
 
+// This code add a new paragraph 
 addbutton.addEventListener('click', function(){
    let paragraph = document.createElement('p');
    paragraph.innerText = inputText.value;
    res.appendChild(paragraph);
    inputText.value = '';
 
+   //This code removes the paragraph by double clicking on the element 
    paragraph.addEventListener('click', function(){
        res.removeChild(paragraph);
    })
