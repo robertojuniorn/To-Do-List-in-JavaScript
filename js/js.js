8e4a6d31069d98fa330f412db8755a5ae685b888
Roberto Simões <robertojuniorn@gmail.com>
2024-01-22 11:44:09 +0200

    New features on Css card

--- js/js.js
+++ js/js.js
@@ -3,7 +3,10 @@ const btn = document.getElementById("btn");
 const inputList = document.getElementById("dados");
 const listaCompleta = document.querySelector('.list-tasks')
 
+
 const taskList = []
+
+// This funtion insert the task in taskList Array
 
 function insertTasks (){
     taskList.push(inputList.value)
@@ -12,13 +15,17 @@ function insertTasks (){
     showTask()
 }
 
+// This function show the task
+
 function showTask (){
     let novaLi = ""
 
     taskList.forEach(task => {
         novaLi = novaLi + `
         <li class="task">
+        <i class='bx bxs-check-circle'></i>
         <p>${task}</p>
+        <i class='bx bxs-trash' ></i>
         </li>
         `
     })
